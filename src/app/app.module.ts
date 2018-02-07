30211da29566b35570bbcd3ee53dc7b83547618d
Grace Achenyo Okolo <grace.okolo15@gmail.com>
2018-02-07 14:17:09 +0200

    Login component initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 //add your pages here
 import { LoginPage } from '../pages/login/login';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
   //route your pages here
-  { path: 'login', component: LoginPage }
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPage
+    LoginPage,
+    LoginComponent
   ],
   imports: [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     ),
+    NgbModule.forRoot(),
     BrowserModule
   ],
   providers: [],
