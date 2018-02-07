016a5f0dbb73466e763ee2474b28e46ae9231b2f
abcher <abel2mesfin@gmail.com>
2018-02-07 12:06:52 +0200

    created route for dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 //add your pages here
 import { LoginPage } from '../pages/login/login';
-
+import { HomePage } from '../pages/home/home.component';
 const appRoutes: Routes = [
   //route your pages here
-  { path: 'login', component: LoginPage }
+  { path: 'login', component: LoginPage },
+  { path: 'home', component: HomePage }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPage
+    LoginPage,
+    HomePage
   ],
   imports: [
     RouterModule.forRoot(
---
+++ src/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+console.log("i am in homepage");
+
+@Component({
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
+})
+export class HomePage {
+
+}
