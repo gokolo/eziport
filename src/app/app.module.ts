b7339917c9b6c54e7e6c87cb77afd6a2803df93e
abcher <abel2mesfin@gmail.com>
2018-02-07 23:57:25 +0200

    implemented send search and booking and transfer request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-//add your pages here
+import { FormsModule } from '@angular/forms';
+
+
+// add your pages here
 import { LoginPage } from '../pages/login/login';
-import { HomePage } from '../pages/home/home.component';
+import { SendSearchFormComponent } from './send-search-form/send-search-form.component';
+import { HomeComponent } from './home/home.component';
+import { TransferRequestFormComponent } from './transfer-request-form/transfer-request-form.component';
 const appRoutes: Routes = [
-  //route your pages here
+  // route your pages here
   { path: 'login', component: LoginPage },
-  { path: 'home', component: HomePage }
+  { path: 'home', component: HomeComponent },
+  { path: 'send', component: SendSearchFormComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPage,
-    HomePage
+    SendSearchFormComponent,
+    HomeComponent,
+    TransferRequestFormComponent
   ],
   imports: [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     ),
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
