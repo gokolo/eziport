f8248ec53e8469d4adb304111c80baa02d889e65
Grace Achenyo Okolo <grace.okolo15@gmail.com>
2018-02-08 13:51:16 +0200

    Admin approval and disapproval working and refreshing page

--- src/app/package/package.component.ts
+++ src/app/package/package.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { Router, Routes } from '@angular/router';
+
 import { Package } from '../package';
 
 @Component({
@@ -11,16 +13,26 @@ import { Package } from '../package';
 export class PackageComponent {
   packages;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
+    this.getPackages();
+   }
+
+   getPackages() {
     this.http.get("http://localhost:8087/get_bookings?username=demoSender").subscribe((data) => this.packages = data);
+
    }
 
    approvePackage(package_id) {
-    this.http.get("http://localhost:8087//update-booking/"+package_id+"/APPROVED").subscribe(data => console.log(data));
+    this.http.get("http://localhost:8087//update-booking/"+package_id+"/APPROVED").subscribe(data => console.log("Package "+package_id+" approved"));
+    this.getPackages();
+    this.router.navigateByUrl(`/admin`);
+
    }
   
    disapprovePackage(package_id) {
-    this.http.get("http://localhost:8087//update-booking/"+package_id+"/DISAPPROVE").subscribe(data => console.log(data));
+    this.http.get("http://localhost:8087//update-booking/"+package_id+"/DISAPPROVE").subscribe(data => console.log("Package "+package_id+" disapproved"));
+    this.getPackages();
+    this.router.navigateByUrl(`/admin`);
    }
 
 }
